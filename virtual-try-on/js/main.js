6bb3cfd4b7238a2938650fb0f997072f4d48339a
MaiChi <t.maichi1510@gmail.com>
2026-08-09 23:12:23 +0900

    second time

--- virtual-try-on/js/main.js
+++ virtual-try-on/js/main.js
@@ -1,1 +1,7 @@
-console.log("connected");
+const tops = [
+  "https://placehold.co/150x180?text=Top+1",
+  "https://placehold.co/150x180?text=Top+2",
+  "https://placehold.co/150x180?text=Top+3",
+];
+let currentTopIndex = 0;
+console.log(tops[currentTopIndex]);
